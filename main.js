a4b2968af9d68407a398c70a207924365042e582
Thomas Fritzsche <tf@noto.de>
2024-10-23 22:24:40 +0200

    Add some HTML input element for call/rst/nr

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
 import { DEFAULT } from "./defaults.js"
 import { Contest } from "./contest.js"
 
-import { Station } from "./station.js"
+//import { Station } from "./station.js"
+
+
+
+const setFocus = id => {
+    document.getElementById(id).focus();
+}
+
+const wipeFields = () => {
+    document.getElementById('call').value = ''
+    document.getElementById('rst').value = ''
+    document.getElementById('nr').value = ''
+    setFocus('call')
+}
+
+const functionKey = () => {
+    document.getElementById('input').addEventListener("keydown", (e) => {
+        console.log(e.code)
+    });
+}
 
 window.onload = () => {
-    Station.NrAsText(599,29)
+    functionKey()
+    wipeFields()
+    // allow only number input on RST and NR
+    var nr_input = document.querySelectorAll('.NR')
+    Array.from(nr_input).forEach(input => {
+        input.addEventListener("beforeinput", e => {
+            const nextVal =
+                e.target.value.substring(0, e.target.selectionStart) +
+                (e.data ?? '') +
+                e.target.value.substring(e.target.selectionEnd)
+            if (!/^\d{0,3}$/.test(nextVal)) {
+                e.preventDefault();
+            }
+            return
+        })
+    })
+
     const start_button = document.getElementById("start")
     start_button.onclick = async () => {
+        wipeFields()
         let ctx = new (window.AudioContext || window.webkitAudioContext)({ sampleRate: DEFAULT.RATE })
         await ctx.audioWorklet.addModule("contest-processor.js");
         const ContestNode = new AudioWorkletNode(
@@ -16,7 +52,7 @@ window.onload = () => {
         ContestNode.connect(ctx.destination);
     }
     const debug_button = document.getElementById("debug")
-    debug_button.onclick = async () => {     
+    debug_button.onclick = async () => {
         let MyContest = new Contest(DEFAULT.RATE)
         let result = new Float32Array(DEFAULT.RATE * 60 * 2)
         MyContest.getBlock(result)
