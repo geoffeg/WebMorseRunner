cfe46b294063a382f28ffbc6585ed211ffcb0c26
Thomas Fritzsche <tf@noto.de>
2024-12-15 19:59:14 +0100

    remove semicolon

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import { DEFAULT, RunMode } from "./defaults.js";
+import { DEFAULT, RunMode } from "./defaults.js"
 
 export class Config {
     static store_key = "_WebMorseKey";
@@ -14,14 +14,14 @@ export class Config {
         this._rit = document.querySelector("#rit")
         this._runmode = document.querySelector("#mode")
         this._activity = document.querySelector("#activity")
-        this._callback = callback;
+        this._callback = callback
 
         this.all = document.querySelectorAll(".watch").forEach(
             (d) =>
                 d.addEventListener("input", (e) => {
-                    this.update();
+                    this.update()
                 }),
-        );
+        )
 
         this._config = {
             my_call: "DJ1TF",
@@ -34,31 +34,31 @@ export class Config {
             rit: 0,
             runmode: RunMode.Single,
             activity: 2,
-        };
-        this.load();
+        }
+        this.load()
     }
 
     update() {
-        this.read_dom();
-        this.updatePileupFields();
-        this.store();
-        this._callback(this._config);
+        this.read_dom()
+        this.updatePileupFields()
+        this.store()
+        this._callback(this._config)
     }
 
     updateRIT(x) {
-        let rit = Number(this._rit.value);
-        this._rit.value = String(rit + x);
-        this.update();
+        let rit = Number(this._rit.value)
+        this._rit.value = String(rit + x)
+        this.update()
     }
 
     store() {
-        localStorage.setItem(Config.store_key, JSON.stringify(this._config));
+        localStorage.setItem(Config.store_key, JSON.stringify(this._config))
     }
     load() {
-        let config_str = localStorage.getItem(Config.store_key);
+        let config_str = localStorage.getItem(Config.store_key)
         if (config_str) {
-            let conf = JSON.parse(config_str);
-            if (conf) this._config = conf;
+            let conf = JSON.parse(config_str)
+            if (conf) this._config = conf
         }
     }
 
@@ -66,36 +66,36 @@ export class Config {
         document.querySelectorAll(".pileup_only").forEach(
             (e) => {
                 if (this._config.runmode === RunMode.Pileup) {
-                    e.classList.remove("pileup_hidden");
-                } else e.classList.add("pileup_hidden");
+                    e.classList.remove("pileup_hidden")
+                } else e.classList.add("pileup_hidden")
             },
-        );
+        )
     }
 
     update_dom() {
-        this._my_call.value = this._config.my_call;
-        this._volume.value = this._config.volume;
-        this._wpm.value = this._config.wpm;
-        this._pitch.value = this._config.pitch;
-        this._time.value = this._config.time;
-        this._qsk.checked = this._config.qsk;
-        this._bandwidth.value = this._config.rx_bandwidth;
-        this._rit.value = this._config.rit;
-        this._runmode.value = String(this._config.runmode);
-        this._activity.value = String(this._config.activity);
-        this.updatePileupFields();
+        this._my_call.value = this._config.my_call
+        this._volume.value = this._config.volume
+        this._wpm.value = this._config.wpm
+        this._pitch.value = this._config.pitch
+        this._time.value = this._config.time
+        this._qsk.checked = this._config.qsk
+        this._bandwidth.value = this._config.rx_bandwidth
+        this._rit.value = this._config.rit
+        this._runmode.value = String(this._config.runmode)
+        this._activity.value = String(this._config.activity)
+        this.updatePileupFields()
     }
 
     read_dom() {
-        this._config.my_call = this._my_call.value.toUpperCase();
-        this._config.volume = this._volume.value;
-        this._config.wpm = this._wpm.value;
-        this._config.pitch = this._pitch.value;
-        this._config.time = this._time.value;
-        this._config.qsk = this._qsk.checked;
-        this._config.rx_bandwidth = this._bandwidth.value;
-        this._config.rit = this._rit.value;
-        this._config.runmode = parseInt(this._runmode.value);
-        this._config.activity = parseInt(this._activity.value);
+        this._config.my_call = this._my_call.value.toUpperCase()
+        this._config.volume = this._volume.value
+        this._config.wpm = this._wpm.value
+        this._config.pitch = this._pitch.value
+        this._config.time = this._time.value
+        this._config.qsk = this._qsk.checked
+        this._config.rx_bandwidth = this._bandwidth.value
+        this._config.rit = this._rit.value
+        this._config.runmode = parseInt(this._runmode.value)
+        this._config.activity = parseInt(this._activity.value)
     }
 }
