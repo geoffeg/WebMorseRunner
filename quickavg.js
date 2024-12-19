b26dba7757093d2ec36f8a709d0e712ab7e3c433
Thomas Fritzsche <tf@noto.de>
2024-12-19 21:34:28 +0100

    quickavg

--- quickavg.js
+++ quickavg.js
@@ -8,23 +8,31 @@ class QuickAverage {
     }
 
     Filter(ARe, AIm) {
-        let re = this._DoFilter(ARe, this.BufRe)
-        let im = this._DoFilter(AIm, this.BufIm)
+        let re = this._DoFilter(ARe, this.ReBufs)
+        let im = this._DoFilter(AIm, this.ImBufs)
         return { Re: re, Im: im }
     }
 
     Reset() {
+        const number_of_buffers = this.FPasses + 1
 
-        this.ReBufs = nil
-        this.SetLength(this.ReBufs, this.FPasses + 1, this.FPoints)
+        this.ReBufs = Array.from(Array(number_of_buffers), _ => new Float32Array(this.FPoints))
+        this.ImBufs = Array.from(Array(number_of_buffers), _ => new Float32Array(this.FPoints))
 
-        this.ImBufs = nil
-        this.SetLength(this.ImBufs, this.FPasses + 1, this.FPoints)
 
-        FScale = Math.pow(this.FPoints, -this.FPasses)
+        this.FScale = Math.pow(this.FPoints, -this.FPasses)
         this.Idx = 0
-        this.PrevIdx = this.FPoints - 1
+        this.PrevIdx = this.FPoints - 1        
+    }
 
+    set passes(value) {
+        this.FPasses = Math.max(1, Math.min(8, value));
+        this.Reset();  
+    }
+
+    set points(value) {
+        this.FPoints = Math.max(1, value);
+        this.Reset();  
     }
 
 }
