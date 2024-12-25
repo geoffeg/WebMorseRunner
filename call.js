94a974dabd5a934356efd070900fa133bb359e93
Thomas Fritzsche <tf@noto.de>
2024-12-25 13:09:51 +0100

    the calls.txt is now cached in local storage

--- call.js
+++ call.js
@@ -1,13 +1,28 @@
 export class Calls {
+
+    static store_key = 'web_morse_runner_calls'
+
     constructor() {
 
     }
 
+   filter_calls(calls) {
+    return calls.split('\n').map( e => e.trim()).filter(
+        (c) => {
+            return c.length >2 && c.length < 15
+        })
+   }
+
     async fetch_calls() {
-        this.calls = (await (await fetch('calls.txt')).text()).split('\n').map( e => e.trim()).filter(
-            (c) => {
-                return c.length >2 && c.length < 8
-            })
+        const localdb__str = localStorage.getItem(Calls.store_key)
+        this.calls = new Array()
+        if (localdb__str) this.calls = this.filter_calls(localdb__str)
+        if (this.calls.length > 0) return 
+        const fetched_calls = await (await fetch('calls.txt')).text()
+        
+        localStorage.setItem(Calls.store_key, fetched_calls)
+        this.calls = this.filter_calls(fetched_calls)
+
     }
 
     get_random_int(min, max) {
